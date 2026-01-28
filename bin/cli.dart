3a5cd31c26da421f1591af0fdc180c8366e87cda
Mona Raj <agrmonaraj@gmail.com>
2026-01-28 13:31:24 +0530

    Implement Wikipedia article search functionality and update dependencies

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
@@ -7,29 +8,52 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
-  }else {
+  } else {
     printUsage();
   }
 }
 
-void searchWikipedia(List<String>? arguments) {
-  final String articleTitle;
+void searchWikipedia(List<String>? arguments) async {
+  late String? articleTitle;
 
-  if (arguments == null || arguments.isEmpty ) {
+  if (arguments == null || arguments.isEmpty) {
     print('Please provide article title');
-    articleTitle = stdin.readLineSync() ?? '';
+    final inputFronStin = stdin.readLineSync();
+    if (inputFronStin == null || inputFronStin.isEmpty) {
+      print('No article title provided. Exiting.');
+      return;
+    }
+    articleTitle = inputFronStin;
   } else {
     articleTitle = arguments.join((' '));
   }
 
   print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+  
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
 }
 
 void printUsage() {
-  print("The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final client = http.Client();
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+  final response = await client.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
 }
