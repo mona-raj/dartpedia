842a82286685086c6aed3b127339bc875bcf5da8
Mona Raj <agrmonaraj@gmail.com>
2026-02-01 19:52:17 +0530

    Update cli.dart to use CommandRunner and HelpCommand

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,9 +1,8 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
+const version = '0.0.1';
+
 void main(List<String> arguments) async {
-  // main is now async and awaits the runner
-  var runner = CommandRunner(); // Create an instance of your new CommandRunner
-  await runner.run(arguments); // Call its run method, awaiting its Future<void>
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
