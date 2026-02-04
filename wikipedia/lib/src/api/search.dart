f492c87c1d16061acaa95a2559e074002bc7ed45
Mona Raj <agrmonaraj@gmail.com>
2026-02-04 11:26:51 +0530

    Implement Wikipedia API calls for search results and searching an article by title

--- wikipedia/lib/src/api/search.dart
+++ wikipedia/lib/src/api/search.dart
@@ -1,1 +1,38 @@
 // This file handles search queries to find articles.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+import '../model/search_results.dart';
+
+Future<SearchResults> search(String searchTerm) async {
+  final http.Client client = http.Client();
+  try {
+    final Uri url = Uri.https(
+      'en.wikipedia.org',
+      '/w/api.php',
+      <String, Object?>{
+        'action': 'opensearch',
+        'format': 'json',
+        'search': searchTerm,
+      },
+    );
+    final http.Response response = await client.get(url);
+    if (response.statusCode == 200) {
+      final List<Object?> jsonData = jsonDecode(response.body) as List<Object?>;
+      return SearchResults.fromJson(jsonData);
+    } else {
+      throw HttpException(
+        '[WikimediaApiClient.getArticleByTitle] '
+        'statusCode=${response.statusCode}, '
+        'body=${response.body}',
+      );
+    }
+  } on FormatException {
+    rethrow;
+  } finally {
+    client.close();
+  }
+}
